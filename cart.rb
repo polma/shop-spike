4ea65478df0ac10e40dd953f9bbd53c5834c0f69
Piotr Olma <perdo.olma@gmail.com>
2013-01-11 23:52:46 +0100

    Add a method for applying discounts

--- cart.rb
+++ cart.rb
@@ -19,5 +19,10 @@ module Shop
       end
       total
     end
+    def apply_discount(percentage)
+      @items.each do |i|
+        i.price = (100.0 - percentage) / 100.0 * i.price
+      end
+    end
   end
 end
