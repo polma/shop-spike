b4e192e890fe1258b5faa4ea32790f190d8bde74
Piotr Olma <perdo.olma@gmail.com>
2013-01-11 23:12:45 +0100

    Modify buyer test to be more ruby-like

--- buyer_test.rb
+++ buyer_test.rb
@@ -1,13 +1,14 @@
 require 'test/unit'
 require './buyer'
 
+include Shop
 
 class BuyerTest < Test::Unit::TestCase
   def test_can_create_buyer_and_set_name
     b = Buyer.new
-    b.setName("Piotrek")
-    b.setSurname("Pmnb")
-    assert_equal b.getName, "Piotrek"
-    assert_equal b.getSurname, "Pmnb"
+    b.name = "Piotrek"
+    b.surname = "Pmnb"
+    assert_equal b.name, "Piotrek"
+    assert_equal b.surname, "Pmnb"
   end
 end
